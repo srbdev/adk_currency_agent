3e6ab7a4936b57ffdf5da78a19fe7451cdfe3925
srbdev <sylvain.rb@gmail.com>
2026-05-12 15:09:05 -0600

    Add minor fixes
    From interacting with CLI client.

--- src/currency_agent/main.py
+++ src/currency_agent/main.py
@@ -13,6 +13,7 @@ from a2a.server.apps import A2AStarletteApplication
 from a2a.server.request_handlers import DefaultRequestHandler
 from a2a.server.tasks import InMemoryTaskStore
 from a2a.types import AgentCard
+from dotenv import load_dotenv
 from google.adk.runners import Runner
 from google.adk.sessions import InMemorySessionService
 
@@ -23,6 +24,8 @@ from currency_agent.agent_executor import CurrencyAgentExecutor
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
+
+load_dotenv()
 
 
 async def start_server(host: str, port: int) -> None:
